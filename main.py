19f34fff5cd5c7413e449bd03fe7de59592cf6bd
AlePatata <alejandra.campos.u@ug.uchile.cl>
2026-04-08 23:00:06 -0400

    Trying Typer pkg

--- main.py
+++ main.py
@@ -1,6 +1,18 @@
-def main():
+import typer
+
+app = typer.Typer()
+
+@app.command()
+def hello():
     print("Hello from frog!")
+
+@app.command()
+def bye(formal = False):
+    if formal:
+        print("Bye Mr. Frog")
+    else:
+        print("chao sapoql")
 
 
 if __name__ == "__main__":
-    main()
+    app()
